cbad022b012bbe6140b34dbefda2e89625c192c1
rmastudillo <you@example.com>
2022-09-26 12:41:20 -0300

    primeros archivos

---
+++ Tarea1/Deck/Card.cs
@@ -0,0 +1,10 @@
+namespace Deck
+{
+  public class Card
+  {
+    public void Hola()
+    {
+      Console.WriteLine("GOLA");
+    }
+  }
+}
--- Tarea1/Program.cs
+++ Tarea1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Deck;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Card hola = new Card();
+hola.Hola();
